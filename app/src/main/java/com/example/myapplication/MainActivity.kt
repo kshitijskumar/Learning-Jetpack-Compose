71e2ab6e9e5b5bd924172ce8840399cb7bf8f363
Kshitij Kumar <kshitij2212@gmail.com>
2021-04-04 01:32:17 +0530

    used spacers and rows in columns

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -35,10 +36,23 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
-        Image(painter = painterResource(id = R.drawable.header), contentDescription = "This image was downloaded")
-        Text(text = "This is string 1")
-        Text(text = "This is string 2")
-        Text(text = "This is string 3")
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = "This image was downloaded",
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { } // ignoring click for now
+        )
+        Spacer(modifier = Modifier.height(24.dp).fillMaxWidth()) // acts as a space between two components
+        Text(text = "This is string 1", fontWeight = FontWeight.ExtraBold)
+        Spacer(modifier = Modifier.height(12.dp).fillMaxWidth())
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "This is string 2")
+            Text(text = "This is string 3")
+        }
     }
 }
 
