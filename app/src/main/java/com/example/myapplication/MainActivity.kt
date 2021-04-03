1afe00bf1739b37fb9133a6d65c9b18f26c5d9db
Kshitij Kumar <kshitij2212@gmail.com>
2021-04-04 01:01:05 +0530

    image added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(painter = painterResource(id = R.drawable.header), contentDescription = "This image was downloaded")
         Text(text = "This is string 1")
         Text(text = "This is string 2")
         Text(text = "This is string 3")
